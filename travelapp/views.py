5ec30a5f29d06c55a1e112e25cf545922ad56f0d
shaharbannissam <shaharban.ta22@gmail.com>
2022-03-23 18:03:40 +0530

    added

---
+++ travelapp/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from . models import place
+from . models import team
+# Create your views here.
+def demo(request):
+  obj=place.objects.all()
+  ob=team.objects.all()
+  return render(request,"index.html",{'result':obj,'res':ob})
+
+#def addition(request):
+ #   x=int(request.GET['num1'])
+  #  y=int(request.GET['num2'])
+   # res=x + y
+    #return render(request,"result.html",{'result':res})
